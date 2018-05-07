9ea9b319286b3b9dded212e5bd33f244ee51fce7
Fenna <fennadewilde@hotmail.com>
2018-05-07 14:45:41 +0200

    recieve data

---
+++ src/Websocket.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import SockJS  from 'sockjs-client';
+import Stomp from '@stomp/stompjs';
+const socket = SockJS('https://app.jouliette.net/stomp/');
+const client = Stomp.over(socket);
+
+class WebSocket extends React.Component {
+    state = {
+    };
+    componentDidMount() {
+        this.setupWebSocket();
+    }
+    setupWebSocket = () => {
+        client.connect('web', 'mnwdTGgQu5zPmSrz', this.connect, console.error, '/');
+    }
+    connect = () => {
+        client.subscribe('/exchange/power/#');
+    }
+    render() {
+        return (
+                <span></span>
+        );
+    }
+}
+export default WebSocket;
