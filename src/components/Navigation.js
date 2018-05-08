b7ba6cd4483dd9a33820b0b9f3181a36625f8a1e
Sam-Guliker <samguliker@hotmail.com>
2018-05-08 15:39:24 +0200

    homepage fix

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,43 +7,36 @@ class Navigation extends React.Component {
       <li>
         <a className="navigation-links" href="/detail">
           <h1>Biogasboot</h1>
-          <div class="hexTop"></div>
-          <div class="hexBottom"></div>
         </a>
       </li>
       <li>
         <a className="navigation-links" href="#">
           <h1>Metabolic lab</h1>
-          <div class="hexTop"></div>
-          <div class="hexBottom"></div>
-        </a>
-      </li>
-      <li>
-        <a className="navigation-links" href="#">
-          <div class="hexTop"></div>
-          <div class="hexBottom"></div>
-        </a>
-      </li>
-      <li>
-        <a className="navigation-links" href="#">
-          <div class="hexTop"></div>
-          <div class="hexBottom"></div>
-        </a>
-      </li>
-      <li>
-        <a className="navigation-links" href="#">
-          <div class="hexTop"></div>
-          <div class="hexBottom"></div>
         </a>
       </li>
       <li>
         <a className="navigation-links" href="#">
           <h1>Metabolic lab</h1>
-          <div class="hexTop"></div>
-          <div class="hexBottom"></div>
+
         </a>
       </li>
-    </ul>);
+      <li>
+        <a className="navigation-links" href="#">
+          <h1>Metabolic lab</h1>
+        </a>
+      </li>
+      <li>
+        <a className="navigation-links" href="#">
+          <h1>Metabolic lab</h1>
+        </a>
+      </li>
+      <li>
+        <a className="navigation-links" href="#">
+          <h1>Metabolic lab</h1>
+          </a>
+      </li>
+    </ul>
+  );
   }
 }
 export default Navigation;
