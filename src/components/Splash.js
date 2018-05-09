315e18cec3d443d500b46e253f9621e565f26d91
Sam-Guliker <samguliker@hotmail.com>
2018-05-09 09:46:14 +0200

    small css update

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Splash extends React.Component {
    render() {
       return (
-        <section>
+        <section className = 'fadeIn'>
           <img src="http://via.placeholder.com/350x150.jpg"/>
           <p> Some awesome content </p>
           <a href="/overview">next</a>
