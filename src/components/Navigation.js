629fa7e738f52c8b324718180eb0c1a633071fcd
Fenna <fennadewilde@hotmail.com>
2018-05-09 12:31:36 +0200

    added text blob

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,22 +3,30 @@ import React from 'react';
 import {data} from "../data/Data";
 
 class Navigation extends React.Component {
-  render() {
-    return (
-      <ul>
-          {
-            data.map((obj, i) => {
-            return (
-                <li key={i}>
-                    <a className="navigation-links" style={{backgroundImage: `url(${obj.imgUrl})`}} href={obj.link}>
-                        <h1>{obj.name}</h1>
-                    </a>
-                </li>
-            );
-          })
-          }
-    </ul>
-  );
-  }
+    render() {
+        return (
+            <section>
+                <div className="text-blob-container">
+                    <img src={require('../images/hipster-chick.svg')} />
+                    <h1>Here you can find important pieces of the ecosystem</h1>
+                </div>
+                <ul>
+                    {
+                        data.map((obj, i) => {
+                            return (
+                                <li key={i}>
+                                    <a className="navigation-links" style={{backgroundImage: `url(${obj.imgUrl})`}}
+                                       href={obj.link}>
+                                        <h2>{obj.name}</h2>
+                                    </a>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </section>
+        );
+    }
 }
+
 export default Navigation;
