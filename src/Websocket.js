1cecfcfedea5d8ffa0afff33c76f420f2ef5a333
Fenna <fennadewilde@hotmail.com>
2018-05-14 16:18:44 +0200

    websocket update

--- src/Websocket.js
+++ src/Websocket.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react';
 import SockJS  from 'sockjs-client';
 import Stomp from '@stomp/stompjs';
-const socket = new SockJS('https://app.jouliette.net/stomp/');
-const client = Stomp.over(socket);
 
 let client;
 
 const endpoints = {
-    'all':           '/exchange/power/#',
-    'Main meter':    '/exchange/power/00',
+    'Greenhouse': '/exchange/power/00',
     'Metabolic lab': '/exchange/power/A0',
-    'Tech boat':     '/exchange/power/A1',
-    'Crossboat':     '/exchange/power/B0',
-    'Cafe':          '/exchange/power/C0',
-    'Boat 1':        '/exchange/power/01',
-    'Boat 2':        '/exchange/power/02',
-    'Boat 3':        '/exchange/power/03',
-    'Boat 4':        '/exchange/power/04',
-    'Boat 5':        '/exchange/power/05',
-    'Boat 6':        '/exchange/power/06',
-    'Boat 7':        '/exchange/power/07',
-    'Boat 8':        '/exchange/power/08',
-    'Boat 9':        '/exchange/power/09',
-    'Boat 10':       '/exchange/power/10',
-    'Boat 11':       '/exchange/power/11',
-    'Boat 12':       '/exchange/power/12',
-    'Boat 13':       '/exchange/power/13',
-    'Boat 14':       '/exchange/power/14'
+    'Urban biorefinery': '/exchange/power/02',
+    'Biogasboat': '/exchange/power/B0',
+    'Cafe de Ceuvel': '/exchange/power/C0',
+    'Community': '/exchange/power/01',
 };
 
 class WebSocket extends Component {
@@ -47,12 +31,15 @@ class WebSocket extends Component {
         const socket = SockJS('https://app.jouliette.net/stomp/');
         client = Stomp.over(socket);
         client.connect('web', 'mnwdTGgQu5zPmSrz', this.connect, console.error, '/');
-        console.log(client);
-
     };
+    update = (d) => {
+        const data = JSON.parse(d.body);
+        this.props.streamCallback(data);
+        };
 
     connect = () => {
-        this.state.subscription = client.subscribe('/exchange/power/#');
+        const endpoint = endpoints[this.props.boat.name];
+        this.state.subscription = client.subscribe(endpoint, this.update);
     };
     render() {
         return (
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,42 +1,76 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Footer from './Footer';
 
 import {data} from '../data/Data';
 import WebSocket from "../Websocket";
 
-const Detail = (props) => {
+class Detail extends Component {
+    state = {
+        heatPumpActive: true,
+        productionActive: true,
+    }
+    getWebSocketData = (data) => {
+        console.log(data);
+        let heatPumpActive = false;
+        let productionActive = false;
+        if (data.heatpump) {
+            heatPumpActive = true;
+        }
+        console.log(data.solar)
+        if (data.solar) {
+            productionActive = true;
+            let heatpump = data.heatpump ? data.heatpump : 0;
+            let diff = data.solar - (data.consumption + heatpump);
+            console.log(typeof data.solar);
+        }
 
-    const boat = data.find(b => b.link === props.match.url);
-    let content;
+        this.setState({
+            heatPumpActive,
+            productionActive
+        });
 
-    if (boat)
-        content =
-          <section className="detail-container">
-              <div className="hexagon detail-hexagon" style={{backgroundImage: `url(${boat.imgUrl})`}}>
-                  <h1>{boat.name}</h1>
-              </div>
-              <ul className="facts-list">
-                  <li>Data</li>
-                  <li className="big">Data</li>
-                  <li>Data</li>
-              </ul>
-              <h2>Description</h2>
-              <p>{boat.detailText}</p>
-              <Footer boat={boat}/>
-              <WebSocket/>
-          </section>
-    else
-        content =
-            <section>
-                <h1>404</h1>
-            </section>;
+    };
 
+    render() {
+        const boat = data.find(b => b.link === this.props.match.url);
+        let content;
 
-    return (
-        <div className="inner">
-            {content}
-        </div>
-    )
-};
+        if (boat) {
+            content =
+                <section className="detail-container">
+                    <div className="hexagon detail-hexagon" style={{backgroundImage: `url(${boat.imgUrl})`}}>
+                        <h1>{boat.name}</h1>
+                    </div>
+                    <ul className="facts-list">
+                        <li className={this.state.heatPumpActive ? "red" : "inactive"}
+                            style={{backgroundImage: `url(${require('../images/heatpump.png')})`}}></li>
+                        <li className={this.state.productionActive ? "big" : "inactive big"}
+                            style={{backgroundImage: `url(${require('../images/production.png')})`}}>
+                        </li>
+                        <li className="red"
+                            style={{backgroundImage: `url(${require('../images/consumption.png')})`}}></li>
+                        <li style={{backgroundImage: `url(${require('../images/ceuvel.png')})`}} className="block"></li>
+                    </ul>
+                    <h2>Description</h2>
+                    <p>{boat.detailText}</p>
+                    <Footer boat={boat}/>
+                    <WebSocket streamCallback={this.getWebSocketData} boat={boat}/>
+                </section>
+        }
+
+        else {
+            content =
+                <section>
+                    <h1>404</h1>
+                </section>;
+        }
+
+        return (
+            <div className="inner">
+                {content}
+            </div>
+        )
+    }
+}
 
 export default Detail;
