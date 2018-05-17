442d58fb83b9f1d42e1c49ebd9cccb55dc3bf9e7
Fenna <fennadewilde@hotmail.com>
2018-05-17 13:44:53 +0200

    deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,10 @@ class Footer extends React.Component {
           <nav>
             <ul>
               <li>
-                <Link style={{backgroundImage: `url(${this.props.boat.previousImgUrl})`}} to={`${process.env.PUBLIC_URL}`+ this.props.boat.previousLink}>{this.props.boat.previousName}</Link>
+                <Link style={{backgroundImage: `url(${this.props.boat.previousImgUrl})`}} to={this.props.boat.previousLink}>{this.props.boat.previousName}</Link>
               </li>
               <li>
-                <Link style={{backgroundImage: `url(${this.props.boat.nextImgUrl})`}} to={`${process.env.PUBLIC_URL}`+ this.props.boat.nextLink}>{this.props.boat.nextName}</Link>
+                <Link style={{backgroundImage: `url(${this.props.boat.nextImgUrl})`}} to={this.props.boat.nextLink}>{this.props.boat.nextName}</Link>
               </li>
             </ul>
           </nav>
